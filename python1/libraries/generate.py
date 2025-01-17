2ed1c333be4ff6ab566c2896288669cb9703807c
Rishi Srivastava <rishi-pt7824@rishi-pt7824.csez.zohocorpin.com>
2025-01-17 11:48:50 +0530

    Completed Libraries

---
+++ python1/libraries/generate.py
@@ -0,0 +1,13 @@
+import random
+
+# coin = random.choice(["Heads", "Tails"])
+# print(coin)
+
+# number = random.randint(1,10)
+# print(number)
+
+# cards = ["Jack", "queen", "King"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
+
