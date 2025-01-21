3ff7b8b4d2d840cb3c06a77b232643354b8cc1a5
Rishi Srivastava <rishi-pt7824@rishi-pt7824.csez.zohocorpin.com>
2025-01-21 13:37:44 +0530

    Completed OOPs

---
+++ python1/OOPs/hat.py
@@ -0,0 +1,11 @@
+from random import choice
+
+class Hat:
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", choice(cls.houses))
+
+
+Hat.sort("Harry")
