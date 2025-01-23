723490bc087395a87e5716e902e818c14e968b1c
Rishi Srivastava <rishijc02@gmail.com>
2025-01-23 16:33:06 +0530

    Decorators

---
+++ random-stuff/caching-decorator.py
@@ -0,0 +1,22 @@
+import time
+
+def cache(func):
+    cache_value = {}
+    def wrapper(*args):
+        if args in cache_value:
+            return cache_value[args]
+        result = func(*args)
+        cache_value[args] = result
+        return result
+    return wrapper
+
+@cache
+def long_running_function(a, b):
+    time.sleep(4)
+    return a+b
+
+
+val1 = [1, 2, 3, 4, 5, 2, 3, 4, 5]
+
+for val in val1:
+    print(long_running_function(val, val))
